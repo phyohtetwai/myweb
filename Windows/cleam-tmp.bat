607fe391628d7d39a458ab4a0e3898f59d886a9c
B14CK D3V1L <43138249+phyohtetwai@users.noreply.github.com>
2026-05-31 16:07:36 +0630

    Delete cleam-tmp.bat

--- Windows/cleam-tmp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 0A
-echo Cleaning temporary files...
-del /q /f /s "%TEMP%\*" >nul 2>&1
-for /d %%x in ("%TEMP%\*") do rd /s /q "%%x" >nul 2>&1
-del /q /f /s "C:\Windows\Temp\*" >nul 2>&1
-for /d %%x in ("C:\Windows\Temp\*") do rd /s /q "%%x" >nul 2>&1
-echo Done!
-pause
